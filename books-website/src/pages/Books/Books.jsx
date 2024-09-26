1b4041c9ed297f2e56d59fd1d540017ceb88d577
Tegar Penemuan <tegarpenemuan0@gmail.com>
2024-09-26 14:41:22 +0700

    Implement endpoint books

--- books-website/src/pages/Books/Books.jsx
+++ books-website/src/pages/Books/Books.jsx
@@ -1,12 +1,42 @@
 import { Button, Container, Table } from "react-bootstrap"
 import { useNavigate } from "react-router-dom"
 import App from "../../layouts/app"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 function Books() {
   const navigate = useNavigate()
+  const [books, setBooks] = useState([])
   const logoutHandler = () => {
+    localStorage.clear('access_token')
+    localStorage.clear('refresh_token')
     navigate('/login')
   }
+
+  useEffect(()=>{
+    async function getData() {
+      try {
+        const res = await axios.get('http://localhost:3001/books', {
+          headers: {
+            'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+          }
+        })
+
+        // console.log(res);
+        
+        setBooks(res.data.data.books)
+      } catch (error) {
+        // console.log(error);
+        if(error.response.status === 401) {
+          localStorage.clear('access_token')
+          localStorage.clear('refresh_token')
+          navigate('/login')
+        }
+      }
+    }
+
+    getData()
+  },[])
 
   return (
     <App>
@@ -21,18 +51,14 @@ function Books() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Liburan di Gunung Kembar</td>
-              <td>Mario</td>
-              <td>123</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Liburan di Gunung Kembar</td>
-              <td>Mario</td>
-              <td>123</td>
-            </tr>
+            {books.map((book, i) => (
+              <tr key={book.id}>
+                <td>{i + 1}</td>
+                <td>{book.title}</td>
+                <td>{book.author}</td>
+                <td>{book.pages}</td>
+              </tr>
+            ))}
           </tbody>
         </Table>
         <Button size="sm" variant="danger" onClick={logoutHandler}>Logout</Button>
