afe7a9a60da915b9ce6aa4e4d429ba36cf9a71ef
Tegar Discovery <70625970+TegarDiscovery@users.noreply.github.com>
2024-12-23 14:07:10 +0700

    Perbaikan hit refresh token lebih dari 1x

--- books-website/src/utils/axios/instance.js
+++ books-website/src/utils/axios/instance.js
@@ -1,42 +1,80 @@
-import axios from 'axios'
+import axios from "axios";
 
-const instance = axios.create()
+const instance = axios.create({
+  baseURL: "http://localhost:3001",
+  timeout: 5000, // Waktu tunggu maksimum 5 detik
+});
+
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error = null) => {
+  while (failedQueue.length) {
+    const [resolve, reject] = failedQueue.shift();
+    if (error) {
+      reject(error);
+    } else {
+      resolve();
+    }
+  }
+};
 
 instance.interceptors.request.use(
-    function(config) {
-        config.headers['Authorization'] = `Bearer ${localStorage.getItem('access_token')}`
-
-        return config
-    },
-    function(error) {
-        return Promise.reject(error)
-    }
-)
+  function (config) {
+    config.headers["Authorization"] = `Bearer ${localStorage.getItem(
+      "access_token"
+    )}`;
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
 instance.interceptors.response.use(
-    function(res) {
-        return res
-    },
-    async function(error){
-        try {
-            if(error.response.status === 401 && !error.config._refreshing){
-                error.config._refreshing = true
-                const res = await instance.post('http://localhost:3001/auth/refresh', {
-                    refresh_token: localStorage.getItem('refresh_token')
-                })
+  function (res) {
+    return res;
+  },
+  async function (error) {
+    const originalRequest = error.config;
 
-                localStorage.setItem('access_token',res.data.data.access_token)
+    if (error.response.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push([resolve, reject]);
+        })
+          .then(() => instance(originalRequest))
+          .catch((err) => Promise.reject(err));
+      }
 
-                const apiRes = await instance(error.config)
+      originalRequest._retry = true;
+      isRefreshing = true;
 
-                return apiRes
-            }
+      try {
+        const res = await instance.post("/auth/refresh", {
+          refresh_token: localStorage.getItem("refresh_token"),
+        });
 
-            return Promise.reject(error)
-        } catch {
-            return Promise.reject(error)
+        if (res.data && res.data.data && res.data.data.access_token) {
+          localStorage.setItem("access_token", res.data.data.access_token);
+          processQueue();
+          return instance(originalRequest);
+        } else {
+          throw new Error("Invalid token response");
         }
+      } catch (err) {
+        processQueue(err);
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("refresh_token");
+        window.location.href = "/login"; // Redirect ke halaman login
+        return Promise.reject(err);
+      } finally {
+        isRefreshing = false;
+      }
     }
-)
 
-export default instance
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
